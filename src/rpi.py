db933bdeb37fa8c3be7504c765aa2aeea46ceab8
Lorand Cheng <lfcheng@usc.edu>
2020-11-03 11:22:19 -0800

    Grovepi library added

---
+++ src/rpi.py
@@ -0,0 +1,10 @@
+import time
+import sys
+import threading
+#GrovePi Modules
+import grovepi
+import grove_rgb_lcd
+sys.path.append('../../Software/Python/')
+# This append is to support importing the LCD library.
+sys.path.append('../../Software/Python/grove_rgb_lcd')
+
