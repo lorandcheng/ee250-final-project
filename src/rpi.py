8d00290dda2c091b3419f14f000b2fe04b170193
Lorand Cheng <lfcheng@usc.edu>
2020-11-04 20:58:44 -0800

    morse code logic, binary tree, press detection

--- src/rpi.py
+++ src/rpi.py
@@ -7,30 +7,90 @@ sys.path.append('../grovepi/Software/Python/')
 # This append is to support importing the LCD library.
 sys.path.append('../grovepi/Software/Python/grove_rgb_lcd')
 
-#GrovePi Modules
+# GrovePi Modules
 import grovepi
 import grove_rgb_lcd
 
-#TODO: Declare port numbers
-LED = 1
-ULTRASONIC = 2
-BUTTON = 3
+#Declare message tools
+from morseCode import Morse_Code_Bin_Tree, Node
+letter = ""
+message = ""
+morse = Morse_Code_Bin_Tree()
 
-#Initialize grovepi
+
+"""
+Declare port numbers, initialize pins, create lock
+"""
+LED = 1 #D1
 grovepi.pinMode(LED,"OUTPUT")
+BUTTON = 3 #D3
 grovepi.pinMode(BUTTON,"INPUT")
+# LCD connected to I2C port
 grove_rgb_lcd.textCommand(0x01) # Clear display
-
-#Initialize threading
 lock = threading.Lock() #define I2C lock
 
 
+
+
+def actAfterPause(duration):
+    """
+    Either end the current letter or insert a space depending on the duration of the pause
+    """
+    global message
+    global letter
+    # Define pause lengths (s)
+    END = 1
+    SPACE = 3
+    # Determine action
+    if END < duration <= SPACE:
+        message += morse.translate_mc_to_letter(letter)
+        letter = ""
+    elif SPACE < duration:
+        message += " "
+
+def actAfterPress(duration):
+    """
+    Either add a dot/dash to the current letter, or send the message depending on the duration of the press
+    """
+    global letter
+    # Define press lengths (s)
+    DASH = 0.4
+    SEND = 3
+    # Determine actino
+    if duration < DASH:
+        letter += "."
+        print(".")
+    elif DASH <= duration < SEND:
+        letter+= "-"
+        print("-")
+    elif SEND < duration:
+        print(message)
+        message = ""
+
+def buttonPressed():
+    """
+    read button value
+    """
+    with lock:
+        return grovepi.digitalRead(BUTTON)
+
+
 if __name__ == '__main__':
-    # while True:
-    #     with lock:
-    #         button = grovepi.digitalRead(BUTTON)
-    #     if(button):
-    #         start = time.time()
-    print(time.time())
-    time.sleep(1)
-    print(time.time())
+    # initialize timer variables
+    pressStart = 0
+    pressDuration = 0
+    pauseStart = 0
+    pauseDuration  = 0
+    # loop forever
+    while True:
+        # if the button was pressed
+        if buttonPressed():
+            pressStart = time.time() # start button timer
+            if pauseStart:
+                pauseDuration = time.time()-pauseStart # calculate length of pause
+                actAfterPause(pauseDuration)
+            while buttonPressed():
+                pass
+            pressDuration = time.time()-pressStart # calculate length of press
+            actAfterPress(pressDuration)
+            pauseStart = time.time() # start pause timer
