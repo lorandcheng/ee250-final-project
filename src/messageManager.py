c9f794d3cfbbe6c79216e1295f77bccf38e8af76
Lorand Cheng <lfcheng@usc.edu>
2020-11-10 22:04:26 -0800

    reorganizing db updating functions into messageManager

--- src/messageManager.py
+++ src/messageManager.py
@@ -1,4 +1,36 @@
 from datetime import datetime
+from config import config
+import psycopg2
 
 class messageManager:
-    pass
+    def __init__(self):
+        """
+        Summary: Class for managing the message history database
+        """
+        print('Starting Message Manager')
+
+    def addMessage(message):
+        """
+        Summary: Adds provided message to the CloudSQL Postgres database
+
+        Args: 
+            message (json): json body of message from rpi containing sender, message body, and timestamp
+        """
+
+        # Obtain the configuration parameters
+        params = config()
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # Create a new cursor
+        cur = conn.cursor()
+        #extract values from message
+        sender = message['sender']
+        body = message['message']
+        timestamp = message['timestamp']
+
+        cur.execute("INSERT INTO MESSAGES (SENDER,MESSAGE,TIMESTAMP) VALUES (%s, %s, %s)", (sender,body,timestamp))
+
+        # For example:cursor.execute("insert into people values (%s, %s)", (who, age))
+        # Update DB and close connection
+        conn.commit()
+        conn.close()
--- src/messageServer.py
+++ src/messageServer.py
@@ -2,8 +2,7 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-from config import config
-import psycopg2
+
 import json
 import messageManager
 
@@ -13,24 +12,7 @@ app = Flask('Cloud Messaging Server')
 HOST = '0.0.0.0'
 PORT = '4200'
 
-def writeToDB(message):
-    # Obtain the configuration parameters
-    params = config()
-    # Connect to the PostgreSQL database
-    conn = psycopg2.connect(**params)
-    # Create a new cursor
-    cur = conn.cursor()
-    #extract values from message
-    sender = message['sender']
-    body = message['message']
-    timestamp = message['timestamp']
 
-    cur.execute("INSERT INTO MESSAGES (SENDER,MESSAGE,TIMESTAMP) VALUES (%s, %s, %s)", (sender,body,timestamp))
-
-    # For example:cursor.execute("insert into people values (%s, %s)", (who, age))
-     # Update DB and close connection
-    conn.commit()
-    conn.close()
 
 
 @app.route('/send-message', methods=['POST'])
@@ -45,7 +27,7 @@ def postMessageCallback():
     # Get the payload containing the sender, message, and timestamp
     payload = request.get_json()
     print(payload)
-    writeToDB(payload)
+    messageManager.addMessage(payload)
     response = {'Response': 'Message sent'}
 
     # The object returned will be sent back as an HTTP message to the requester
@@ -60,5 +42,6 @@ if __name__ == '__main__':
     # }
     # writeToDB(test)
 
+    messageManager = messageManager();
     # Start the flask app
     app.run(debug=False, host=HOST, port=PORT)
