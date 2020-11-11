9249616aadf0c45bded7d20fbb3ee303aac4be4b
Lorand Cheng <lfcheng@usc.edu>
2020-11-10 21:11:37 -0800

    db connection tests

--- src/messageServer.py
+++ src/messageServer.py
@@ -2,14 +2,36 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-
+from config import config
+import psycopg2
 import json
 import messageManager
+
 
 app = Flask('Cloud Messaging Server')
 
 HOST = '0.0.0.0'
 PORT = '4200'
+
+def writeToDB(message):
+    # Obtain the configuration parameters
+    params = config()
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(**params)
+    # Create a new cursor
+    cur = conn.cursor()
+    #extract values from message
+    sender = message['sender']
+    body = message['message']
+    timestamp = message['timestamp']
+
+    cur.execute("INSERT INTO MESSAGES (SENDER,MESSAGE,TIMESTAMP) VALUES (%s, %s, %s)", (sender,body,timestamp))
+
+    # For example:cursor.execute("insert into people values (%s, %s)", (who, age))
+     # Update DB and close connection
+    conn.commit()
+    conn.close()
+
 
 @app.route('/send-message', methods=['POST'])
 def postMessageCallback():
@@ -30,4 +52,13 @@ def postMessageCallback():
     return json.dumps(response)
 
 if __name__ == '__main__':
+    
+    test = {
+        'sender': 'Lorand',
+        'message': 'testing message',
+        'timestamp': 'today'
+    }
+    writeToDB(test)
+
+    # Start the flask app
     app.run(debug=False, host=HOST, port=PORT)
