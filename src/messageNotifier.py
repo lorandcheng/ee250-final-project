de604a23a28f5975239fae7cec09691415fdd74b
Lorand Cheng <lfcheng@usc.edu>
2020-11-11 22:35:50 -0800

    testing

--- src/messageNotifier.py
+++ src/messageNotifier.py
@@ -39,5 +39,5 @@ if __name__ == '__main__':
     """
     from constants import SERVER
     messageClient = messageHandler("rpi",SERVER)
-    notifier = Notifier(messageClient,1))
+    notifier = Notifier(messageClient,1)
     print(notifier.getMessages())
