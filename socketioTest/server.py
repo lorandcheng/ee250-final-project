8fd571ba71785f79270baf83699e5421e9a85c9f
Lorand Cheng <lfcheng@usc.edu>
2020-11-13 18:28:48 -0800

    testing json parsing from frontend data

--- socketioTest/server.py
+++ socketioTest/server.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+import json
+from datetime import datetime
+from flask import Flask, request, render_template
 from flask_socketio import SocketIO
 
-HOST = '0.0.0.0'
+HOST = 'localhost'
 PORT = '4200'
 
 app = Flask(__name__)
@@ -10,6 +12,17 @@ socketio = SocketIO(app)
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/test', methods=['POST'])
+def postMessage():
+    payload = request.get_data().decode('utf-8')
+    payload = json.loads(payload)
+    payload['timestamp'] = str(datetime.now())
+    print(payload)
+    response = {'Response': 'Message sent'}
+
+    # The object returned will be sent back as an HTTP message to the requester
+    return json.dumps(response)
 
 @socketio.on('initial-connect')
 def handle_connection(message):
