a37f45e46f25f184d451939bd63d75cf20a4e0b4
Lorand Cheng <lfcheng@usc.edu>
2020-11-14 21:32:51 -0800

    fractional time

--- src/messageNotifier.py
+++ src/messageNotifier.py
@@ -9,10 +9,10 @@ def convertDatetimeTz(dt, tz1, tz2):
         tz1 = pytz.timezone(tz1)
         tz2 = pytz.timezone(tz2)
 
-        dt = datetime.strptime(dt,"%Y-%m-%d %H:%M:%S")
+        dt = datetime.strptime(dt,"%Y-%m-%d %H:%M:%S.%f")
         dt = tz1.localize(dt)
         dt = dt.astimezone(tz2)
-        dt = dt.strftime("%Y-%m-%d %H:%M:%S")
+        dt = dt.strftime("%Y-%m-%d %H:%M:%S.%f")
 
         return dt
 class Notifier():
