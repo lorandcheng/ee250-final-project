37bc5b364421881d2ecffd235f229378883cf51f
Lorand Cheng <lfcheng@usc.edu>
2020-11-14 21:17:37 -0800

    test

--- src/messageNotifier.py
+++ src/messageNotifier.py
@@ -9,7 +9,7 @@ def convertDatetimeTz(dt, tz1, tz2):
         tz1 = pytz.timezone(tz1)
         tz2 = pytz.timezone(tz2)
 
-        dt = datetime.datetime.strptime(dt,"%Y-%m-%d %H:%M:%S")
+        dt = datetime.strptime(dt,"%Y-%m-%d %H:%M:%S")
         dt = tz1.localize(dt)
         dt = dt.astimezone(tz2)
         dt = dt.strftime("%Y-%m-%d %H:%M:%S")
