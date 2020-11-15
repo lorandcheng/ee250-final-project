9a3d08e655417f1b7119cc3744c93642ed6d567f
Lorand Cheng <lfcheng@usc.edu>
2020-11-14 21:06:05 -0800

    test

--- src/messageServer.py
+++ src/messageServer.py
@@ -71,20 +71,6 @@ def historyCallback():
 def handleConnection(message):
     print(message)
 
-import datetime
-import pytz
-
-def convertDatetimeTimezone(dt, tz1, tz2):
-    tz1 = pytz.timezone(tz1)
-    tz2 = pytz.timezone(tz2)
-
-    dt = datetime.datetime.strptime(dt,"%Y-%m-%d %H:%M:%S")
-    dt = tz1.localize(dt)
-    dt = dt.astimezone(tz2)
-    dt = dt.strftime("%Y-%m-%d %H:%M:%S")
-
-    return dt
-
 if __name__ == '__main__':
     
     # test = {
